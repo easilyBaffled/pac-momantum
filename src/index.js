a8dacd49926e3832db4b720c62afe8b6009b0fa1
Dan Michaelis <dmichaelis0+github@gmail.com>
2018-08-02 20:40:49 -0400

    move svg

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 import {
-  Composite,
-  Engine,
-  Render,
-  World,
-  Bodies,
-  Vector,
-  Events,
-  Svg,
-  Vertices
-} from "matter-js";
-import { colors, lighter, darker } from "./colors";
-import GameWorld from "./gameWorld";
+    Composite,
+    Engine,
+    Render,
+    World,
+    Bodies,
+    Vector,
+    Events,
+    Svg,
+    Vertices
+} from 'matter-js';
+
+import { colors, lighter, darker } from './colors';
+import GameWorld from './gameWorld';
 import {
-  vectors,
-  isCollisionWith,
-  setVelocity,
-  applyForceAtTarget,
-  createElementFromHTML
-} from "./util";
-import ramp from "../assets/ramp.svg";
+    vectors,
+    isCollisionWith,
+    setVelocity,
+    applyForceAtTarget,
+    createElementFromHTML
+} from './util';
+
 // Docs http://brm.io/matter-js/docs/
 
 /************
@@ -30,21 +31,21 @@ const acceleration = world.unit * 0.7;
 const maxSpeed = world.unit * 4;
 const friction = acceleration * 0.01;
 
-document.getElementById("app").insertAdjacentHTML(
-  "beforebegin",
-  `<pre>
+document.getElementById('app').insertAdjacentHTML(
+    'beforebegin',
+    `<pre>
     <code id="data">
       ${JSON.stringify(
-        {
-          acceleration,
-          maxSpeed,
-          friction
-        },
-        null,
-        4
+          {
+              acceleration,
+              maxSpeed,
+              friction
+          },
+          null,
+          4
       )}
     </code>
-  </pre>`.replace(/(^\s+)(?!")/gm, "")
+  </pre>`.replace(/(^\s+)(?!")/gm, '')
 );
 
 /****************
@@ -52,164 +53,171 @@ document.getElementById("app").insertAdjacentHTML(
  ****************/
 
 const Pac = (x, y) => {
-  const pac = Bodies.circle(
-    x,
-    y,
-    world.unit * 1.5, // radius
-    {
-      frictionAir: friction,
-      restitution: 0.5,
-      label: "pac",
-      density: world.unit,
-      render: {
-        fillStyle: darker(colors.yellow, 10)
-      }
-    },
-    20 // maxSides
-  );
+    const pac = Bodies.circle(
+        x,
+        y,
+        world.unit * 1.5, // radius
+        {
+            frictionAir: friction,
+            restitution: 0.5,
+            label: 'pac',
+            density: world.unit,
+            render: {
+                fillStyle: darker(colors.yellow, 10)
+            }
+        },
+        20 // maxSides
+    );
 
-  pac.collisionHandler = isCollisionWith(pac)({
-    pellet() {
-      setVelocity(Vector.mult(this.velocity, world.unit / 3))(this);
-    } // best keep the multiplier small, world.unit sends it off screen fast // setVelocity works much better than applyForce, not sure why
-  });
+    pac.collisionHandler = isCollisionWith(pac)({
+        pellet() {
+            setVelocity(Vector.mult(this.velocity, world.unit / 3))(this);
+        } // best keep the multiplier small, world.unit sends it off screen fast // setVelocity works much better than applyForce, not sure why
+    });
 
-  return pac;
+    return pac;
 };
 
 const Pellet = (x, y) => {
-  const pellet = Bodies.circle(
-    x,
-    y,
-    world.unit * 0.75,
-    {
-      render: {
-        fillStyle: lighter(colors.yellow, 10)
-      },
-      label: "pellet",
-      isSensor: true // prevents pac from bouncing off the pellet
-      // isStatic: true // unclear if this helps in this case
-    },
-    20
-  );
+    const pellet = Bodies.circle(
+        x,
+        y,
+        world.unit * 0.75,
+        {
+            render: {
+                fillStyle: lighter(colors.yellow, 10)
+            },
+            label: 'pellet',
+            isSensor: true // prevents pac from bouncing off the pellet
+            // isStatic: true // unclear if this helps in this case
+        },
+        20
+    );
 
-  pellet.collisionHandler = isCollisionWith(pellet)({
-    pac() {
-      Composite.remove(world.engine.world, this);
-    }
-  });
+    pellet.collisionHandler = isCollisionWith(pellet)({
+        pac() {
+            Composite.remove(world.engine.world, this);
+        }
+    });
 
-  return pellet;
+    return pellet;
 };
 
 const Ghost = (x, y) => {
-  const ghost = Bodies.circle(x, y, world.unit * 2, {
-    friction: 0.05,
-    frictionAir: 0.03,
-    frictionStatic: 0,
-    restitution: 0.5,
-    label: "ghost",
-    sleepThreshold: 3000,
-    density: world.unit,
-    render: {
-      fillStyle: colors.red
-    }
-  });
+    const ghost = Bodies.circle(x, y, world.unit * 2, {
+        friction: 0.05,
+        frictionAir: 0.03,
+        frictionStatic: 0,
+        restitution: 0.5,
+        label: 'ghost',
+        sleepThreshold: 3000,
+        density: world.unit,
+        render: {
+            fillStyle: colors.red
+        }
+    });
 
-  ghost.collisionHandler = isCollisionWith(ghost)({
-    wall() {
-      if (Vector.magnitude(this.velocity) > 3) {
-        Composite.remove(world.engine.world, this);
-      }
-    }
-  });
+    ghost.collisionHandler = isCollisionWith(ghost)({
+        wall() {
+            if (Vector.magnitude(this.velocity) > 3) {
+                Composite.remove(world.engine.world, this);
+            }
+        }
+    });
 
-  return ghost;
+    return ghost;
 };
 
 /*********************************
     SVG -> Body
  *********************************/
-const halfPipe = createElementFromHTML(ramp).getElementById("curved-path");
-const halfPipeVertices = Svg.pathToVertices(halfPipe, 10);
-const scaledHalfPipe = Vertices.scale(halfPipeVertices, 1.2, 1.4);
+const halfpipeTemplate = document.querySelector('#halfpipe-svg');
+const halfpipePath = document
+    .importNode(halfpipeTemplate.content, true)
+    .querySelector('path');
 
+const halfpipeVertices = Svg.pathToVertices(halfpipePath, 10);
+const scaledHalfPipe = Vertices.scale(halfpipeVertices, 1.2, 1.4);
 const vertexSets = [scaledHalfPipe];
 
 const terrain = Bodies.fromVertices(
-  134,
-  65,
-  vertexSets,
-  {
-    isStatic: true,
-    render: {
-      fillStyle: colors.blue
-    }
-  },
-  true
+    134,
+    65,
+    vertexSets,
+    {
+        isStatic: true,
+        render: {
+            fillStyle: colors.blue
+        }
+    },
+    true
 );
 
 const pac = Pac(world.unit * 1.5, world.height * 0.75);
 
 world.addBodies(
-  ...Array.from({ length: 4 }, (_, i) =>
-    Pellet(world.unit * 1.5 + i, world.height * 0.65 - i * 20)
-  ),
-  ...Array.from({ length: 4 }, (_, i) =>
-    Pellet(250 + i, world.height * 0.25 + i * 25)
-  ),
-  Ghost(262.5, world.height * 0.5),
-  Ghost(262.5, world.height * 0.5 + 40),
-  pac,
-  terrain
+    ...Array.from({ length: 4 }, (_, i) =>
+        Pellet(world.unit * 1.5 + i, world.height * 0.65 - i * 20)
+    ),
+    ...Array.from({ length: 4 }, (_, i) =>
+        Pellet(250 + i, world.height * 0.25 + i * 25)
+    ),
+    Ghost(262.5, world.height * 0.5),
+    Ghost(262.5, world.height * 0.5 + 40),
+    pac,
+    terrain
 );
 
 Engine.run(world.engine);
 Render.run(world.render);
 
-Events.on(world.engine, "collisionStart", event =>
-  event.pairs.forEach(({ bodyA, bodyB }) => {
-    bodyA.collisionHandler && bodyA.collisionHandler(event);
-    bodyB.collisionHandler && bodyB.collisionHandler(event);
-  })
+Events.on(world.engine, 'collisionStart', event =>
+    event.pairs.forEach(({ bodyA, bodyB }) => {
+        bodyA.collisionHandler && bodyA.collisionHandler(event);
+        bodyB.collisionHandler && bodyB.collisionHandler(event);
+    })
 );
 
 /***************
     Controles
  ***************/
 const keyForceMapping = {
-    ArrowUp: Vector.mult(vectors.up, acceleration),
-    ArrowDown: Vector.mult(vectors.down, acceleration),
-    ArrowLeft: Vector.mult(vectors.left, acceleration),
-    ArrowRight: Vector.mult(vectors.right, acceleration)
-  },
-  controlForcesMapper = key => keyForceMapping[key] || vectors.zero;
+        ArrowUp: Vector.mult(vectors.up, acceleration),
+        ArrowDown: Vector.mult(vectors.down, acceleration),
+        ArrowLeft: Vector.mult(vectors.left, acceleration),
+        ArrowRight: Vector.mult(vectors.right, acceleration)
+    },
+    controlForcesMapper = key => keyForceMapping[key] || vectors.zero;
 
 const applyImpulseSpeed = (impulse, clamp, velocity) => {
-  if (impulse === 0) return velocity;
+    if (impulse === 0) return velocity;
 
-  const { x, y } = Vector.mult(velocity, impulse);
+    const { x, y } = Vector.mult(velocity, impulse);
 
-  return {
-    x: Math.abs(x) > clamp ? Math.sign(x) * clamp : x,
-    y: Math.abs(y) > clamp ? Math.sign(y) * clamp : y
-  };
+    return {
+        x: Math.abs(x) > clamp ? Math.sign(x) * clamp : x,
+        y: Math.abs(y) > clamp ? Math.sign(y) * clamp : y
+    };
 };
 
 let keys = [];
-document.body.addEventListener("keyup", ({ key }) => keys.push(key));
+document.body.addEventListener('keyup', ({ key }) => keys.push(key));
 
-Events.on(world.engine, "beforeTick", event => {
-  if (keys.includes(" ")) setVelocity(vectors.zero)(pac);
-  else {
-    const nextAccelVector = keys.reduce((finalVector, key) => {
-      const directionVector = controlForcesMapper(key);
-      const vector = applyImpulseSpeed(acceleration, maxSpeed, directionVector);
-      return Vector.add(finalVector, vector);
-    }, vectors.zero);
+Events.on(world.engine, 'beforeTick', event => {
+    if (keys.includes(' ')) setVelocity(vectors.zero)(pac);
+    else {
+        const nextAccelVector = keys.reduce((finalVector, key) => {
+            const directionVector = controlForcesMapper(key);
+            const vector = applyImpulseSpeed(
+                acceleration,
+                maxSpeed,
+                directionVector
+            );
+            return Vector.add(finalVector, vector);
+        }, vectors.zero);
 
-    Vector.magnitude(nextAccelVector) > 0 &&
-      applyForceAtTarget(nextAccelVector)(pac);
-  }
-  keys = [];
+        Vector.magnitude(nextAccelVector) > 0 &&
+            applyForceAtTarget(nextAccelVector)(pac);
+    }
+    keys = [];
 });
